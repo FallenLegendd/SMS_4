658264736d64b0b2c4ee9740fb353d141fa1ceaf
FallenLegendd <borisov.saveliy@mail.ru>
2023-12-19 03:04:38 +0700

    lab4

---
+++ OSMS_lab4.cpp
@@ -0,0 +1,100 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define N 5
+
+void shiftSequence(int* sequence, int length) {
+    int tmp = sequence[length - 1];
+    for (int i = 0; i < length; i++) {
+        sequence[length - i - 1] = sequence[length - i - 2];
+    }
+    sequence[0] = tmp;
+}
+
+void calculateAutocorrelation(int* sequence, int* shiftedSequence, float* correlation, int length) {
+    float result = 0;
+    for (int i = 0; i < length; i++) {
+        result += (sequence[i] == shiftedSequence[i]) ? 1.0 : -1.0;
+    }
+    *correlation = result / length;
+}
+
+void calculateCrosscorrelation(int* sequence1, int* sequence2, float* correlation, int length) {
+    float result = 0.0;
+    for (int i = 0; i < length; i++) {
+        result += sequence1[i] * sequence2[i];
+    }
+    *correlation = result;
+}
+
+void generateGoldSequence(int* registerX, int* registerY, int* sequence, int length) {
+    for (int i = 0; i < length; i++) {
+        sequence[i] = (registerX[N - 1] + registerY[N - 1]) % 2;
+        shiftSequence(registerX, N);
+        shiftSequence(registerY, N);
+    }
+}
+
+void printShiftTable(int shift, int* shiftedSequence, int* sequence, int length) {
+    int i, j;
+    float correlation;
+    printf("\nShift");
+    for (i = 1; i <= length; i++) {
+        printf("|%2d", i);
+    }
+    printf("|Correlation|");
+    for (i = 0; i <= shift; i++) {
+        printf("\n");
+        printf("| %2d ", i);
+        for (j = 0; j < length; j++) {
+            printf("|%2d", shiftedSequence[j]);
+        }
+        calculateAutocorrelation(sequence, shiftedSequence, &correlation, length);
+        printf("|%5.1f     |", correlation);
+        shiftSequence(shiftedSequence, length);
+    }
+    printf("\n-------------------------------------\n");
+}
+
+int main() {
+    int i = 0;
+    int sequenceLength = pow(2, N) - 1;
+
+    int* sequence1 = (int*)malloc(sequenceLength * sizeof(int));
+    int* sequence2 = (int*)malloc(sequenceLength * sizeof(int));
+    int* shiftedSequence = (int*)malloc(sequenceLength * sizeof(int));
+
+    int registerX[N] = { 0, 0, 0, 1, 0 };
+    int registerY[N] = { 0, 1, 0, 0, 1 };
+    int registerX1[N] = { 0, 0, 0, 1, 1 };
+    int registerY1[N] = { 0, 0, 1, 0, 0 };
+
+    printf("\nFirst Gold's sequence :\n");
+    generateGoldSequence(registerX, registerY, sequence1, sequenceLength);
+    for (i = 0; i < sequenceLength; i++) {
+        printf("%d", sequence1[i]);
+    }
+    printf("\n\n");
+    for (i = 0; i < sequenceLength; i++) {
+        shiftedSequence[i] = sequence1[i];
+    }
+    printShiftTable(sequenceLength, shiftedSequence, sequence1, sequenceLength);
+
+    printf("\nSecond Gold's sequence :\n");
+    generateGoldSequence(registerX1, registerY1, sequence2, sequenceLength);
+    for (i = 0; i < sequenceLength; i++) {
+        printf("%d", sequence2[i]);
+    }
+    printf("\n\n");
+
+    float correlation;
+    calculateCrosscorrelation(sequence1, sequence2, &correlation, sequenceLength);
+    printf("\nCorrelation between the first and second sequence >> %.0f\n", correlation);
+
+    free(sequence1);
+    free(sequence2);
+    free(shiftedSequence);
+
+    return 0;
+}
---
+++ lab4.m
@@ -0,0 +1,100 @@
+
+N = 5; %Количество регистров
+len_pos = 2^N - 1; %Длина последовательности
+%задаем регистры
+register_x = [0, 0, 0, 1, 0];
+register_y = [0, 1, 0, 0, 1];
+register_x1 = [0, 0, 0, 1, 1];
+register_y1 = [0, 0, 1, 0, 0];
+
+% Генерация последовательности Голда и измененной последовательности
+dis = sprintf("\nFirst Gold''s sequencen\n");
+random_sequence = GoldSeq(register_x, register_y, len_pos);
+
+dis2 = sprintf("\nSecond Gold''s sequence (x = 3, y = 5):\n");
+
+modified_sequence = GoldSeq(register_x1, register_y1, len_pos);
+
+%выводим последовательности на экран
+for i = 1:len_pos
+    dis = sprintf("%s %d", dis, random_sequence(i));
+    dis2 = sprintf("%s %d", dis2, modified_sequence(i));
+end
+disp(dis);
+disp(newline);
+disp(dis2);
+%копируем первую последовательность на массив для сдвига
+shift_random_sequence = random_sequence;
+
+% Находим автокорреляцию через xcorr(x,x), тк встроенная функция не работает
+[cor, lag]=xcorr(random_sequence,random_sequence);
+dis = sprintf("\nСдвиг");
+for i = 1:len_pos
+    dis = sprintf("%s|%2d", dis,i);
+end
+dis = sprintf("%s|корреляция|", dis);
+
+%половина массива xcorr
+t = (0:31);
+corrMass = (0:31);
+%считаем автокорреляцию вручную
+for i = 0:len_pos
+    dis = sprintf('%s\n', dis);
+    dis = sprintf("%s| %2d ",dis, i);
+    for j = 1:len_pos
+        dis = sprintf("%s|%2d", dis, shift_random_sequence(j));
+    end
+    %[acf,lags] = autocorr(random_sequence);
+	%Функция для рассчета автокорреляции вручную
+    corr = corrXY(random_sequence, shift_random_sequence, len_pos);
+    	%заполнение массива значениями корреляции
+	corrMass(i+1) = corr/len_pos;
+    dis = sprintf("%s| %5.2f", dis, corr/len_pos);
+    %Сдвиг последовательности
+    shift_random_sequence = circshift(random_sequence, [0, i+1]);
+end
+disp(dis);
+% расчет корреляции между первой и второй последовательностями
+[cor1, lag1]=xcorr(random_sequence,modified_sequence);
+dis = sprintf("\nВзаимная корреляция между первоначальной и новосозданной последовательностью >> %.0f", cor1(len_pos));
+disp(dis);
+%Вывод графиков
+subplot(2, 1, 1);
+plot(t,corrMass);
+xlabel("Сдвиг");
+ylabel("Amplitude");
+title("Ручная автокорреляция");
+subplot(2, 1, 2);
+plot(lag,cor);
+xlabel("lag");
+ylabel("Amplitude");
+title("Замена автокорреляции xcorr(x,x)");
+
+%Функция для генерации последовательности голда
+function sequence = GoldSeq(register_x, register_y, len_pos)
+    sequence = zeros(1, len_pos);
+    for i = 1:len_pos
+        sequence(i) = mod(register_x(5) + register_y(5), 2);
+        [register_x, register_y] = shift(register_x, register_y);
+    end
+end
+%Функция для рассчета автокорреляции
+function corr = corrXY(random_sequence, shift_random_sequence, len_pos)
+    corr = 0;
+    for i = 1:len_pos
+        if random_sequence(i) == shift_random_sequence(i)
+            corr = corr+1;
+        else 
+            corr = corr-1;
+        end
+    end
+end
+%Функция для сдвига регистров
+function [register_x, register_y] = shift(register_x, register_y)
+    res_x = mod(register_x(3) + register_x(4), 2);
+    res_y = mod(register_y(2) + register_y(3), 2);
+    register_x = circshift(register_x, [0, 1]);
+    register_x(1) = res_x;
+    register_y = circshift(register_y, [0, 1]);
+    register_y(1) = res_y;
+end
